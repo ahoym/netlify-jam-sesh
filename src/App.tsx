6f731af2c61453a9026813392676d21654bc90d2
Malcolm Ahoy <malcolmahoy@Malcolms-MacBook-Pro.local>
2020-05-18 12:25:49 -0400

    Add basic apollo graphql client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+
+const HELLO_WORLD = gql`
+  {
+    hello
+  }
+`;
+type HelloQuery = {
+  hello: string;
+};
+
+function Loader() {
+  return <p>Loading...</p>;
+}
+
+function ErrorState() {
+  return (
+    <p>
+      Something went wrong! Try again, and if the issue persists please contact
+      supprt.
+    </p>
+  );
+}
+
+function EmptyState() {
+  return <p>No data found.</p>;
+}
+
+function TestQuery() {
+  const { data, error, loading } = useQuery<HelloQuery>(HELLO_WORLD);
+
+  if (loading) return <Loader />;
+  if (error) return <ErrorState />;
+  if (!data) return <EmptyState />;
+
+  return <p>{data.hello}</p>;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <TestQuery />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
